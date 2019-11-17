14aec94718103e5e035ebd79d2f6adc801f8788b
kavi22493@gmail.com <kavi22493@gmail.com>
2019-11-17 02:46:52 +0000

    Added button below each question. But there is issue in redirecting. Need to be fixed

--- flaskr/index.py
+++ flaskr/index.py
@@ -11,14 +11,16 @@ from flask import (
 
 bp = Blueprint('index', __name__, url_prefix='/')
 
+
 @bp.route('/', methods=["GET"])
 def index():
     return render_template('index.html')
 
+
 @bp.route('/query/', methods=["GET"])
 def query():
     question_id = request.args.get('q_id')
-
+    print(question_id)
     sparql_endpoint = 'http://127.0.0.1:3030/kde/query'
     sparql = SPARQLWrapper(sparql_endpoint)
     query = getattr(sys.modules[__name__], "generate_q%s_query" % question_id)()
