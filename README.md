fa4d1d5538a265fff2e44ae41b81de2b0c183644
Rohan Anand <thededlier@gmail.com>
2019-11-21 16:49:36 +0000

    updated questions

--- README.md
+++ README.md
@@ -11,16 +11,3 @@ export FLASK_APP=flaskr
 export FLASK_ENV=development
 flask run
 ```
-
-## Questions
-
-1. Number of nobel laureate per country (University & Nobel Laureate)
-2. University ranking to which a nobel laureate is affiliated to in 2016
-3. Statistic of country for top n universities in the world in 2016
-4. number of nobel prize winning papers for a country
-5. ratio of enrollment to primary vs students in university in 2016 for a country
-6. ResearchRating of a university which has a nobel laureates affiliated to it, in year 2016
-7. Country of a nobel laureate
-8. GDP & Gold Medals of a country in the year 2000
-9. Total number of Gold medals won by a country in the toughest sport
-10. Male Female ratio of olympic winners in toughest sport between 2000-2008 (This can be converted to a Pie Chart showing the ratio of first 5 toughest sports)
--- flaskr/query_set.py
+++ flaskr/query_set.py
@@ -1,4 +1,5 @@
-# 1. Number of nobel laureate per country
+# 1. Get names of Nobel laureates for given country
+# Inputs: Country
 def generate_q1_query(param_0, param_1, param_2):
     query = """
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -7,18 +8,21 @@ def generate_q1_query(param_0, param_1, param_2):
         PREFIX ex: <http://example.org/>
         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-        SELECT  (SAMPLE(?univname) AS ?UNIVERSITY) (SAMPLE(?ranking) AS ?RANK)
+        SELECT (UCASE(?laureateUniversityName) AS ?UNIVERSITY) (UCASE(?laureateName) AS ?LAUREATE_NAME)
         WHERE {
-            ?laureate exont:affiliated_to_university ?college.
-            ?university dbo:name ?univname
-            FILTER ( regex (str(?college), str(?univname), "i") ).
-            ?university exont:has_rank ?rank.
-            ?rank exont:world_rank ?ranking.
-        }GROUP BY ?univname ?ranking
-    """
+          	?country dbo:name '%s'.
+            ?university exont:situatedIn ?country.
+            ?university exont:universityName ?universityname.
+            ?laureate exont:affiliatedToUniversity ?univ.
+            ?univ exont:universityName ?laureateUniversityName.
+            FILTER ( regex (str(?universityname), str(?laureateUniversityName), "i") ).
+            ?laureate foaf:name ?laureateName
+        }
+    """ % (param_0)
     return query
 
-# 2. University ranking to which a nobel laureate is affiliated to in 2016
+# 2. University ranking to which a Nobel laureate is affiliated to in 2016
+# INPUT: Laureate Name
 def generate_q2_query(param_0, param_1, param_2):
     query = """
         PREFIX foaf: <http://xmlns.com/foaf/0.1/>
@@ -27,119 +31,237 @@ def generate_q2_query(param_0, param_1, param_2):
         PREFIX ex: <http://example.org/>
         PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-        SELECT  (SAMPLE(?univname) AS ?UNIVERSITY) (SAMPLE(?ranking) AS ?RANK)
-        WHERE {
-            ?laureate exont:affiliated_to_university ?college.
-            ?university dbo:name ?univname
-            FILTER ( regex (str(?college), str(?univname), "i") ).
-            ?university exont:has_rank ?rank.
-            ?rank exont:world_rank ?ranking.
-        }GROUP BY ?univname ?ranking
-    """
+        select (UCASE(?laureateUniversityName) AS ?UNIVERSITY_NAME) (?universityRank AS ?WORLD_RANKING)
+
+        where
+        {
+        	?laureate  foaf:name '%s'.
+            ?laureate exont:affiliatedToUniversity ?laureateUniversity.
+            ?laureateUniversity exont:universityName ?laureateUniversityName.
+            ?university exont:situatedIn ?country.
+            ?university exont:universityName ?universityname.
+            FILTER ( regex (str(?universityname), str(?laureateUniversityName), "i") ).
+            ?university exont:hasRank ?universityRankObj.
+            ?universityRankObj exont:worldRank ?universityRank
+        }
+    """ % (param_0)
 
     return query
 
-# 3. Statistic of country for top n universities in the world in 2016
+# 3. Titles of nobel prize winning papers for a country
+# INPUT: Country
 def generate_q3_query(param_0, param_1, param_2):
     query = """
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
         PREFIX exont: <http://example.org/ontology/>
-        PREFIX ex: <http://example.org/>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-        SELECT  (SAMPLE(?univname) AS ?UNIVERSITY) (SAMPLE(?ranking) AS ?RANK)
+        SELECT (UCASE(?laureateUnivName) AS ?UNIVERSITY_NAME) (UCASE(?researchPaperTitle) AS ?PAPER_TITLE)
         WHERE {
-            ?laureate exont:affiliated_to_university ?college.
-            ?university dbo:name ?univname
-            FILTER ( regex (str(?college), str(?univname), "i") ).
-            ?university exont:has_rank ?rank.
-            ?rank exont:world_rank ?ranking.
-        }GROUP BY ?univname ?ranking
-    """
+          	?country dbo:name '%s'.
+            ?university exont:situatedIn ?country.
+            ?university exont:universityName ?universityname.
+            ?laureate exont:affiliatedToUniversity ?univ.
+            ?univ exont:universityName ?laureateUnivName.
+            FILTER ( regex (str(?universityname), str(?laureateUnivName), "i") ).
+            ?laureate exont:hasPublished ?researchPaper.
+            ?researchPaper dbo:title ?researchPaperTitle
+        }
+    """ % (param_0)
 
     return query
 
-# 4. number of nobel prize winning papers for a country
+# 4. Ratio of students in university vs population of the country in 2016
+# INPUT Country
 def generate_q4_query(param_0, param_1, param_2):
     query = """
-
-    """
-
-    return query
-
-# ratio of enrollment to primary vs students in university in 2016 for a country
-def generate_q5_query(param_0, param_1, param_2):
-    query = """
-
-    """
-
-    return query
-
-
-# 6. ResearchRating of a university which has a nobel laureates affiliated to it, in year 2016
-def generate_q6_query(param_0, param_1, param_2):
-    query = """
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
         PREFIX exont: <http://example.org/ontology/>
-        PREFIX ex: <http://example.org/>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
+        SELECT (?universityname AS ?UNIVERSITY_NAME) (?numberOfStudents AS ?TOTAL_NO_OF_STUDENTS) (?populationCount AS ?TOTAL_POPULATION)
 
-        SELECT  (SAMPLE(?univname) AS ?UNIVERSITY) (SAMPLE(?rating) AS ?RESEARCH_RATING)
         WHERE {
-            ?laureate exont:affiliated_to_university ?college.
-            ?university dbo:name ?univname
-            FILTER ( regex (str(?college), str(?univname), "i") ).
-            ?university exont:has_rank ?rank.
-            ?rank exont:defined_by ?params.
-            ?params exont:research_rating ?rating.
-        }GROUP BY ?univname ?rating
-    """
+          	?country dbo:name '%s'.
+            ?university exont:situatedIn ?country.
+            ?university exont:universityName ?universityname.
+            ?university exont:hasRank ?universityRankObj.
+            ?universityRankObj exont:worldRank ?universityWorldRank.
+            ?universityRankObj exont:definedBy ?universityRankingParams.
+            ?universityRankingParams exont:numberOfStudents ?numberOfStudents.
+        		{
+
+          			SELECT ?populationCount
+        					WHERE {
+         							?country dbo:name '%s'.
+          							?country exont:developmentIndicator ?devParam.
+          							?devParam exont:statName 'Population, total'.
+          							?devParam exont:statYear 2016.
+          							?devParam exont:statValue ?populationCount.
+        							}
+          		}
+        }ORDER BY DESC (?numberOfStudents)
+    """ % (param_0, param_0)
+
+    return query
+
+# 5. Research rating of a university which has a Nobel laureates affiliated to it, in year 2016
+# INPUT: Laureate names
+def generate_q5_query(param_0, param_1, param_2):
+    query = """
+        PREFIX exont: <http://example.org/ontology/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+        SELECT (?laureateUniversityName AS ?UNIVERSITY_NAME) (?universityCountry AS ?UNIVERSITY_SITUATED_IN) (?researchRating AS ?RESEARCH_RATING)
+        WHERE
+        {
+          	?laureate  foaf:name '%s'.
+            ?laureate exont:affiliatedToUniversity ?laureateUniversity.
+            ?laureateUniversity exont:universityName ?laureateUniversityName.
+            ?university exont:situatedIn ?countryObj.
+            ?countryObj dbo:name ?universityCountry.
+            ?university exont:universityName ?universityname.
+            FILTER ( regex (str(?universityname), str(?laureateUniversityName), "i") ).
+            ?university exont:hasRank ?universityRankObj.
+            ?universityRankObj exont:definedBy ?universityRankDefinedByObj.
+            ?universityRankDefinedByObj exont:researchRating ?researchRating.
+
+        }
+    """ % (param_0)
 
     return query
 
 
-# 7. Country of a nobel laureate
+# 6. Identifying relation between a country’s GDP and total number of Gold Medals won by that country in Olympics in the specified year.
+# INPUT: Year
+def generate_q6_query(param_0, param_1, param_2):
+    query = """
+        PREFIX exont: <http://example.org/ontology/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
+
+        SELECT (?countryName AS ?COUNTRY) (count(?athlete) AS ?GOLD_MDALS) (?gdp AS ?Gross_Domestic_Product)
+
+        WHERE
+        {
+        	?athlete exont:medalWon 'Gold'.
+            ?athlete exont:participatedIn ?olympicEventObj.
+            ?olympicEventObj exont:yearOfEvent %s.
+            ?athlete  dbo:sportCountry ?athleteSportCountryObj.
+            ?athleteSportCountryObj dbo:name ?countryName.
+          {
+
+          	SELECT ?gdp ?countryName
+    			WHERE {
+    						?country dbo:name ?countryName.
+    						?country exont:developmentIndicator ?devParam.
+    						?devParam exont:statName 'GDP (current US$)'.
+    						?devParam exont:statYear %s.
+    						?devParam exont:statValue ?gdp.
+    			      }
+        	}
+        }GROUP BY ?countryName ?gdp ORDER BY DESC (count(?athlete))
+    """ % (param_0, param_0)
+
+    return query
+
+
+# 7. Total number of medals won by a country in the toughest sport.
+# INPUT: MEDAL TYPE ie. [GOLD,SILVER,BRONZE]
 def generate_q7_query(param_0, param_1, param_2):
     query = """
+        PREFIX exont: <http://example.org/ontology/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
 
-    """
+        SELECT (COUNT(?athlete) AS ?TOTAL_MEDALS) (?athleteSportCountryName AS ?COUNTRY) (?toughSportName AS ?SPORTS_DISCIPLINE)
+
+        WHERE {
+
+          ?toughSport exont:difficultyLevel 1.
+          ?toughSport dbo:name ?toughSportName.
+          ?olympicEvent exont:hasSportDiscipline ?toughSportName.
+          ?athlete exont:participatedIn ?olympicEvent.
+          ?athlete exont:medalWon '%s'.
+          ?athlete dbo:sportCountry ?athleteSportCountryObj.
+          ?athleteSportCountryObj dbo:name ?athleteSportCountryName.
+
+        }GROUP BY ?athleteSportCountryName ?toughSportName ORDER BY DESC (COUNT(?athlete))
+    """ % (param_0)
 
     return query
 
-# 8. GDP & Gold Medals of a country in the year 2000
+# 8. Male female ratio of Olympic winners based on specified level of difficulty of the sport. (1 Being the toughest sport)
+# INPUT: Year and Level of difficulty
 def generate_q8_query(param_0, param_1, param_2):
     query = """
+        PREFIX exont: <http://example.org/ontology/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
 
-    """
+        SELECT (COUNT(?athlete) AS ?NUMBER_OF_WINNERS) (?sex AS ?GENDER) (?toughSportName AS ?SPORTS_DISCIPLINE)
+
+        WHERE {
+
+          ?toughSport exont:difficultyLevel %s.
+          ?toughSport dbo:name ?toughSportName.
+          ?olympicEvent exont:hasSportDiscipline ?toughSportName.
+          ?olympicEvent exont:yearOfEvent %s.
+          ?athlete exont:participatedIn ?olympicEvent.
+          ?athlete dbo:sex ?sex.
+
+        } GROUP BY ?sex ?toughSportName
+    """ % (param_1, param_0)
 
     return query
 
-# 9. Total number of Gold medals won by a country in the toughest sport
+# 9. What is the total number of Olympics medals won by a country in a given year?
+# Inputs: MEDAL TYPE, COUNTRY, YEAR
 def generate_q9_query(param_0, param_1, param_2):
     query = """
         PREFIX exont: <http://example.org/ontology/>
         PREFIX dbo: <http://dbpedia.org/ontology/>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-        SELECT COUNT(?athlete)
+
+        SELECT  (COUNT(?athlete) AS ?TOTAL_MEDALS)
         WHERE {
-            ?country dbo:name "%s".  ?country exont:country_code ?countryCode.
-            ?subject exont:difficulty_level 1.
-            ?subject dbo:name ?sportname.
-            ?discipline exont:has_sport_discipline ?sportname.
-            ?athlete exont:participated_in ?discipline.
-            ?athlete exont:country_code ?countryCode.
-            ?athlete exont:medal_won "Gold".
+          ?athlete exont:medalWon '%s'.
+          ?athlete exont:participatedIn ?olympicEvent.
+          ?olympicEvent exont:yearOfEvent %s.
+          ?athlete dbo:sportCountry ?sportCountry.
+          ?sportCountry exont:countryCode ?countryCode.
+          {
+            SELECT DISTINCT ?countryCode
+            WHERE {
+            		?country dbo:name '%s'.
+          			?country exont:countryCode ?countryCode.
+        		}
+        	}
         }
-    """ % param_0
+    """ % (param_0, param_2, param_1)
     return query
 
-# 10. Male Female ratio of olympic winners in toughest sport between 2000-2008 (This can be converted to a Pie Chart showing the ratio of first 5 toughest sports)
-def generate_q10_query(params):
+# 10. Comparison between the research rating and the total number of nobel laureates of top 20 universities in the world.
+# No input but graph
+def generate_q10_query(param_0, param_1, param_2):
     query = """
+        PREFIX exont: <http://example.org/ontology/>
+        PREFIX dbo: <http://dbpedia.org/ontology/>
 
+        SELECT  (?universityName AS ?UNIVERSITY_NAME)  (?universityWorldRank AS ?WORLD_RANK) (?researchRating AS ?RESEARCH_RATING) (COUNT(?laureate) AS ?NOBEL_LAUREATE_COUNT) (?countryName AS ?COUNTRY)
+
+        WHERE {
+        	?university exont:hasRank ?universityRankObj.
+        	?universityRankObj exont:worldRank ?universityWorldRank.
+        							FILTER (?universityWorldRank < 21).
+          ?universityRankObj exont:definedBy ?universityRankParamObj.
+          ?universityRankParamObj exont:researchRating ?researchRating.
+        	?university exont:universityName ?universityName.
+
+          ?laureate exont:affiliatedToUniversity ?laureateUniversityObj.
+          ?laureateUniversityObj exont:universityName ?laureateUniversityName.
+          FILTER ( regex (str(?universityName), str(?laureateUniversityName), "i") ).
+
+          ?university exont:situatedIn ?countryObj.
+          ?countryObj dbo:name ?countryName.
+
+
+        } GROUP BY ?universityName ?universityWorldRank ?researchRating ?countryName ORDER BY (?universityWorldRank)
     """
 
     return query
